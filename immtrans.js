1542ef9eec05351ce228101d5db4c88149284a56
yamada321 <125833118+yamada321@users.noreply.github.com>
2023-09-17 16:16:32 -0400

    add alt+b translating input field

--- immtrans.js
+++ immtrans.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         imm trans
-// @version      0.2
+// @version      0.2.1
 // @include *
-// @description  alt+a to trigger. imm trans but short enough for trivial audit, it just calls google translate. Translate headers (all) and <p> (8 in each go). Google translate api url adpated from https://addons.mozilla.org/en-US/firefox/addon/translation-selected-text/?utm_content=addons-manager-reviews-link&utm_medium=firefox-browser&utm_source=firefox-browser which i find very useful. 
+// @description  alt+a to translate page, alt+b to translate input, see comments below. Google translate api call adpated from https://addons.mozilla.org/en-US/firefox/addon/translation-selected-text/?utm_content=addons-manager-reviews-link&utm_medium=firefox-browser&utm_source=firefox-browser which i find very useful.
 // @run-at       document-start
 // ==/UserScript==
 
@@ -11,10 +11,10 @@
     let domContentLoadedFlag = false;
     let cnt_now = -1;
 
-       function translateText(text, callback) {
+    function translateText(to_lang, text, callback) {
        var translation_promise = function(){
            const url = "https://translate.googleapis.com/translate_a/single?client=gtx&dt=t&dt=bd&dj=1&sl=auto";
-           return fetch(url + "&tl=en&q=" + encodeURIComponent(text)).then(
+           return fetch(url + "&tl=" + to_lang + "&q=" + encodeURIComponent(text)).then(
 		   r => r.ok ? r.json().then(j => j.sentences.map(s => s.trans).join("")) : "Error: " + r.statusText,
 		   e => "Error: " + e.message);
        }
@@ -29,7 +29,7 @@
 
             for (const paragraph of paragraphs) {
                 const originalText = paragraph.textContent;
-                translateText(originalText, translation => {
+                translateText("en", originalText, translation => {
                     //console.log(`Original Text: ${originalText}`);
                     //console.log(`Translation: ${translation}`);
                     //console.log('---');
@@ -48,7 +48,7 @@
                     continue;
                 }
                 const originalText = paragraph.textContent;
-                translateText(originalText, translation => {
+                translateText("en", originalText, translation => {
                     paragraph.innerHTML += `<span style='background-color:#fff4d9 !important; color:black !important;'><br>▷${translation}▬<br><br>`;
                 });
                 cnt++;
@@ -60,15 +60,38 @@
 
     }
 
+    function trans_input(){
+        const inputField = document.activeElement;
+        const origin = inputField.value;
+        const tmp = origin.split(/\s+/);
+        const targetLang = tmp[0];
+        translateText(targetLang, tmp.slice(1).join(" "), translation=>{
+            inputField.value = translation;
+            }
+        );
+    }
+
 
     window.addEventListener('DOMContentLoaded', function() {
         domContentLoadedFlag = true;
     });
     document.addEventListener('keydown', function(event) {
-        // Check if Alt key and A key are pressed
+        // Translate whole page.
+	// At first trigger, translate h1 through h4 and first 8 p tags. 
+	// Afterwards, translate 8 more paragraphs at each time. 
         if (event.altKey && event.key === 'a' && domContentLoadedFlag) {
             event.preventDefault(); // Prevent the default browser behavior
             processParagraphs();
+        }
+    });
+    document.addEventListener('keydown', function(event) {
+        // Translate input: the input is prefix with the target language code, 
+	// e.g. "en my text" for translating to English (original lang 
+	// is up to auto detection). This prefix will be stripped. 
+	// FIXME: prompt failure when timeout, e.g. cn is not a valid prefix.
+        if (event.altKey && event.key === 'b' && domContentLoadedFlag) {
+            event.preventDefault(); // Prevent the default browser behavior
+            trans_input();
         }
     });
 })();
